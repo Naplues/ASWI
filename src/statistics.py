74ba93deba7a03ded92ada890c61e2d7c03d4fd8
= <=>
2022-01-09 23:36:23 +0800

    add measure_consecutive_feature in statistics.py

--- src/statistics.py
+++ src/statistics.py
@@ -2,6 +2,16 @@
 from helper import *
 import pandas as pd
 
+# F71, F3, F15
+golden_feature_names = ['F25', 'F72',  # 'F71',
+                        'F104', 'F105', 'F101', 'F65', 'F68',
+                        'F126', 'F41',
+                        # 'F3', 'F15',
+                        'F20', 'F21', 'F22', 'F94',
+                        'F77',
+                        'F110', 'F116',
+                        'F115', 'F117', 'F120', 'F123', 'category']
+
 
 def data_summary():
     """
@@ -60,7 +70,6 @@ def export_golden_dataset():
     F123 -> 116 : average lifetime for warning type, average value for feature F100 for a warning type \n
     :return:
     """
-    # F71, F3, F15
     golden_feature_names = ['F25', 'F72', 'F71',
                             'F104', 'F105', 'F101', 'F65', 'F68',
                             'F126', 'F41',
@@ -69,7 +78,6 @@ def export_golden_dataset():
                             'F77',
                             'F110', 'F116',
                             'F115', 'F117', 'F120', 'F123', 'category']
-
     for project in PROJECT:
         make_path(f'{data_path}/{project}/golden/')
         for x in range(1, 6):
@@ -93,11 +101,13 @@ def export_golden_dataset():
 # 如果数据本身相似，是否有必要进行预测
 def get_warning_set(path):
     warning_set = set()
+    warning_list = list()
     data = read_data_from_file(path)
     for line in data:
         ss = line.split(',', maxsplit=5)
         warning_set.add(line.replace(ss[4], ''))
-    return warning_set
+        warning_list.append(line.replace(ss[4], ''))
+    return warning_set, warning_list
 
 
 def measure_consecutive_data():
@@ -105,8 +115,8 @@ def measure_consecutive_data():
     for project in PROJECT:
         print(f'{project}', end=', ')
         for x in range(1, 5):
-            w_set_1 = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x}.csv')
-            w_set_2 = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x + 1}.csv')
+            w_set_1, _ = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x}.csv')
+            w_set_2, _ = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x + 1}.csv')
             intersection = w_set_1.intersection(w_set_2)
             ratio = round(len(intersection) / len(w_set_2), 4)
             print(f'{len(w_set_1)} & {len(w_set_2)} => {len(intersection)}, {ratio}', end=', ')
@@ -114,10 +124,50 @@ def measure_consecutive_data():
     pass
 
 
+def measure_consecutive_feature():
+    print(f'Project, v1 & v2 => O12, Ratio, v2 & v3 => O23, Ratio, v3 & v4 => O34, Ratio, v4 & v5 => O45, Ratio')
+    for feature in golden_feature_names:
+        print(f'========== {feature} ==========')
+        for project in PROJECT:
+            print(f'{project}', end='\t')
+            for x in range(1, 5):
+                # 两版本的警报数据
+                w_set_1, w_list_1 = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x}.csv')
+                w_set_2, w_list_2 = get_warning_set(f'{data_path}/{project}/warnings/warningInfo{x + 1}.csv')
+
+                # 两版本重合的警报数据
+                intersection = list(w_set_1.intersection(w_set_2))
+                # 关联两个版本相同的警报索引
+                index_map = list()
+                for instance in intersection:
+                    cur_index = [w_list_1.index(instance), w_list_2.index(instance)]
+                    if len(cur_index) != 2:
+                        continue
+                    index_map.append(cur_index)
+
+                # 两版本的警报特征
+                data_1 = pd.read_csv(f'{data_path}/{project}/features/totalFeatures{x}.csv')
+                data_2 = pd.read_csv(f'{data_path}/{project}/features/totalFeatures{x + 1}.csv')
+                # print(len(w_set_1), len(w_list_1), len(data_1), len(w_set_2), len(w_list_2), len(data_2))
+                overlap = 0
+                for index in index_map:
+                    if index[0] >= len(data_1) or index[1] >= len(data_2):
+                        continue
+                    f1, f2 = data_1[feature][index[0]], data_2[feature][index[1]]
+                    if f1 == f2:
+                        overlap += 1
+                ratio = round(overlap / len(intersection), 4)
+                print(f'{overlap} / {len(intersection)} => {ratio}', end=', ')
+            print()
+            # break
+    pass
+
+
 def main():
     # data_summary()
     # export_golden_dataset()
-    measure_consecutive_data()
+    # measure_consecutive_data()
+    measure_consecutive_feature()
     pass
 
 
